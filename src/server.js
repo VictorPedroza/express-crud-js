3c8d7d3b13601811c1baa2facef742899e9f8b69
VictorPedroza <victor.pedroza@protonmail.com>
2026-03-25 14:28:57 -0300

    :sparkles: feat(setup): Create and Init application instance

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+const app = require("./app/app");
+
+function startServer() {
+    const baseLog = `
+======================================
+    ✅ API is Running Successfully
+--------------------------------------
+
+======================================
+    `
+
+    // Inicializa o Servidor
+    app.listen(8000, () => {
+        console.log(baseLog);
+    })
+}
+
+startServer();
