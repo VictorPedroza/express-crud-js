d8ec7584c416277a8cf9663240bd28ee14b44453
VictorPedroza <victor.pedroza@protonmail.com>
2026-03-25 14:56:52 -0300

    :zap: feat(setup): Add initial configs on setup config

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const app = express();
 
+const setup = require("@config/core/setup");
+app.use(setup);
+
 module.exports = app;
---
+++ src/config/core/setup.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+// Configurações base da aplicação
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }))
+
+module.exports = router;
