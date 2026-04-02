60b5d1152785a422db9bde28527c29ce0d24c943
VictorPedroza <victor.pedroza@protonmail.com>
2026-04-02 08:32:21 -0300

    ✨ feat:(database): Add query function with sql and params  #1

--- src/config/database/db.config.js
+++ src/config/database/db.config.js
@@ -3,9 +3,9 @@ const { env } = require("@config/env");
 
 /**
  * Database - Class responsável por gerenciar a conexão com o Banco de Dados
- * 
+ *
  * @class
- * 
+ *
  * @author Vicor Pedroza <victor.pedroza@protonmail.com>
  * @since 2026-04-01
  * @version 1.0.0
@@ -17,12 +17,12 @@ class Database {
 
   /**
    * connect - Método para realizar a conexão com o Banco de Dados
-   * 
+   *
    * @async
    * @function connect
-   * 
+   *
    * @returns {Promise<{success: boolean, message: string, error?: string}>} Resultado da conexão
-   * 
+   *
    * @example
    * const connection = await database.connect();
    * if (connection.success) {
@@ -30,7 +30,7 @@ class Database {
    * } else {
    *  console.error(connection.error || connection.message);
    * }
-   * 
+   *
    * **/
   async connect() {
     // Validação para evitar múltiplas conexões
@@ -90,6 +90,47 @@ class Database {
       };
     }
   }
+
+  // * Método de query para facilitar as operações no banco de dados
+  async query(query, params) {
+    // Inicializa a conexão se ainda não estiver conectada
+    if (!this.promisePool) {
+      await this.connect();
+    }
+    let connection;
+
+    // Tenta executar a query e retorna o resultado ou erro
+    try {
+      // Obtém uma conexão do pool
+      connection = await this.promisePool.getConnection();
+
+      // Executa a query com os parâmetros fornecidos
+      const [result] = await connection.execute(query, params);
+      // Retorna o resultado da query
+      return {
+        success: true,
+        data: result,
+      };
+    } catch (err) {
+      // Verifica o ambiente
+      const isProd = env.environment === "production";
+      // Loga o erro no console (com detalhes) e retorna uma mensagem genérica para produção
+      console.error(`[Database Query Error]: ${err.message}`, { sql, params });
+
+      // Retorna o erro. (Sem detalhes em produção)
+      return {
+        success: false,
+        message: "Database query failed.",
+        error: isProd ? undefined : err.message,
+      };
+    } finally {
+      // Garante que a conexão seja liberada
+      if (connection) connection.release();
+    }
+  }
+
+  // TODO: Implementar método de teste de conexão
+  // TODO: Implementar método de fechamento de conexão
 }
 
 module.exports = Database;
