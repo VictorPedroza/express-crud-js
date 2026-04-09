c038386b1e8521336d9060226b7e40fa8f9c52ae
VictorPedroza <victor.pedroza@protonmail.com>
2026-04-09 19:45:16 -0300

    ✨ feat:(repository): Add findByName to product service find by name product #2

--- src/modules/product/repositories/ProductRepository.js
+++ src/modules/product/repositories/ProductRepository.js
@@ -5,7 +5,7 @@ const { BaseRepository } = require("@app/repositories")
  *
  * @author Victor Pedroza <victor.pedroza@protonmail.com>
  * @since 2026-04-03
- * @version 1.0.0
+ * @version 1.1.0
  * 
  * @param {Object} model - Modelo da entidade que define a estrutura da tabela
  * @param {Object} database - Conexão/instância do banco de dados
@@ -14,6 +14,21 @@ const { BaseRepository } = require("@app/repositories")
 class ProductRepository extends BaseRepository {
     constructor(model, database){
         super(model, database);
+    }
+
+    async findByName(name) {
+        try {
+            
+        const [result] = await this.database.query(
+            `SELECT * FROM \`${this.table}\` WHERE name = ? LIMIT 1`,
+            [name]
+        );
+
+        return { success: true, data: result[0] || null };
+        } catch (err) {
+            console.error(`[${this.table} Repository] Error to find by name:`, err);
+            return { success: false, error: "Error to find by name" }
+        }
     }
 };
 
