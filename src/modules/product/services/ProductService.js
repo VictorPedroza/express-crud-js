b8b2b0c095050181987fb264728e9cdfa40d7a32
VictorPedroza <victor.pedroza@protonmail.com>
2026-04-13 20:31:59 -0300

    ✨ feat:(service): Init update service with id and data params

--- src/modules/product/services/ProductService.js
+++ src/modules/product/services/ProductService.js
@@ -2,13 +2,13 @@ const { AppError } = require("@shared/utils");
 
 /**
  * ProductService - Classe de serviços que lidam com a regra de negócio da aplicação
- * 
+ *
  * @author Victor Pedroza <victor.pedroza@protonmail.com>
  * @since 2026-04-09
  * @version 1.0.0
- * 
+ *
  * @param {Object} productRepository - Repository de Produtos com funções de bases
- * 
+ *
  **/
 class ProductService {
   constructor(productRepository) {
@@ -17,48 +17,85 @@ class ProductService {
 
   /**
    * create - Serviço para criar produto na aplicação
-   * 
+   *
    * @param {Object} data - Objeto de dados do produto
    * @param {string} data.name - Nome do produto
    * @param {string} [data.description] - Descrição do Produto (Opcional)
    * @param {number} data.price - Preço do Produto
-   * @param {string} data.category - Categoria do Produto {"Electronics" | "Clothing" | "Books" | "Home" | "Toys"}
+   * @param {string} data.category - Categoria de Produtos {"Electronics" | "Clothing" | "Books" | "Home" | "Toys"}
    * @param {number} data.stock - Valor em estoque
-   * 
+   *
    * @throws {AppError} Lança uma instância do AppError na aplicação
-   * 
+   *
    * @returns {{success: boolean, message: string}} Retorna uma mensagem e o status da operação
-   * 
+   *
    * @example
-   * const result = await productService.create({ 
-   *    name: "Produto", 
-   *    price: 10, category: 
-   *    "Eletronics", 
-   *    stock: 10 
+   * const result = await productService.
+   *    name: "Produto",
+   *    price: 10,
+   *    category: "Eletronics",
+   *    stock: 10
    * });
-   * 
+   *
    * if (result.success) {
    *    console.log(result.message)
    * }
-   * 
+   *
    **/
   async create(data) {
     const existing = await this.productRepository.findByName(data.name);
 
     if (existing.success) {
-      throw AppError.badRequest("Product already exists", { data: existing.data });
+      throw AppError.badRequest("Product already exists", {
+        data: existing.data,
+      });
     }
 
     const result = await this.productRepository.create(data);
 
     if (!result.success) {
-      throw AppError.badRequest("Error creating product");
+      throw new AppError({
+        message: result.error,
+      });
     }
 
     return {
       success: true,
-      message: "Product created successfully"
+      message: "Product created successfully",
     };
+  }
+
+  async update(id, data) {
+    const existing = await this.productRepository.findById(id);
+
+    if (!existing.success) {
+      throw new AppError({
+        message: result?.error,
+      });
+    }
+
+    if (!existing.data) {
+      throw AppError.badRequest("Product not exists", {
+        id
+      });
+    }
+
+    if (!data || Object.keys(data).length === 0) {
+      throw AppError.badRequest("No data provided for update");
+    }
+
+    const result = await this.productRepository.update(id, data);
+    if (!result.success) {
+      throw AppError.badRequest("Error updating produc", {
+        error: result?.error
+      });
+    }
+
+    return {
+      success: true,
+      message: "Product updated successfully.",
+      data: result.data
+    }
   }
 }
 
